cdb57b6f9b8f74881db7870a65103d8842fd4fba
dbiesecke <dbiesecke@gmail.com>
2022-04-14 08:15:53 +0000

    update

--- src/template-m3u.ts
+++ src/template-m3u.ts
@@ -6,11 +6,12 @@ export default ({
 }: {
   baseUrl: string
   redirects: Redirect[]
-}) => () => `${redirects
+}) => () => `#EXTM3U
+${redirects
     .filter((redirect: Redirect) => !!redirect.path)
     .map(
       (redirect: Redirect) => `
-#EXTINF:-1 tvg-chno="-1" tvg-id="" tvg-name="${redirect.path}" tvg-logo="" group-title="",${redirect.path.slice(1)}
+#EXTINF:-1 tvg-chno="-1" tvg-id="" tvg-name="${redirect.path.slice(1)}" tvg-logo="" group-title="",${redirect.path.slice(1)}
 ${redirect.redirect}
               `,
     )
