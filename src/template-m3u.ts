b86d655a5aae7a5cdb44c410b4228ccec319b900
dbiesecke <dbiesecke@gmail.com>
2022-04-14 10:16:12 +0000

    fix channel names

--- src/template-m3u.ts
+++ src/template-m3u.ts
@@ -10,7 +10,7 @@ export default ({
 ${redirects
     .filter((redirect: Redirect) => !!redirect.path)
     .map(
-      (redirect: Redirect) => `#EXTINF:-1 tvg-chno="-1" tvg-id="" tvg-name="${redirect.path.slice(1)}" tvg-logo="" group-title="",${redirect.path.slice(1)}
+      (redirect: Redirect,myindex) => `#EXTINF:-1 tvg-chno="-1" tvg-id="${myindex}" tvg-name="${redirect.path.slice(1)}" tvg-logo="" group-title="",${redirect.path.slice(1)}
 ${redirect.redirect}`,
     )
     .join('\n')}
--- src/template-msx.ts
+++ src/template-msx.ts
@@ -9,6 +9,6 @@ export default ({
 }) => () => `{ "type": "list",    "headline": "Playlist",    "template": {      "type": "control",      "layout": "0,0,12,1",      "color": "msx-glass"    },    "items": [${redirects
       .filter((redirect: Redirect) => !!redirect.path)
       .map(
-        (redirect: Redirect,myindex) => `{ "label": "${redirect.path.slice(1)}-${myindex}", "icon": "msx-yellow:folder","action": "content:${redirect.redirect}" }`,
+        (redirect: Redirect) => `{ "label": "${redirect.path.slice(1)}", "icon": "msx-yellow:folder","action": "content:${redirect.redirect}" }`,
       )
       .join(',')}] }`
